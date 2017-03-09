ef18e72b51d6ac31acb13e0e846271f84469eae3
Matthew Miller <mnmiller1@me.com>
2017-03-09 19:48:07 +1000

    Setup the main breeding system.

--- src/main/kotlin/com/me4502/spacebeetleneat/struct/Network.kt
+++ src/main/kotlin/com/me4502/spacebeetleneat/struct/Network.kt
@@ -2,26 +2,25 @@ package com.me4502.spacebeetleneat.struct
 
 import com.me4502.spacebeetleneat.INPUTS
 import com.me4502.spacebeetleneat.MAX_NODES
-import com.me4502.spacebeetleneat.OUTPUT_NAMES
+import com.me4502.spacebeetleneat.OUTPUT_INDEX
 import com.me4502.spacebeetleneat.sigmoid
 
 class Network {
     val neurons : MutableMap<Int, Neuron> = HashMap()
 
-    fun evaluate(inputs : MutableList<Double>) : Map<String, Boolean> {
-        inputs.add(1.0)
+    fun evaluate(inputs : MutableList<Int>) : MutableMap<Int, Boolean> {
         if (inputs.size != INPUTS) {
-            println("Wrong input format.")
+            println("Wrong input format. Got ${inputs.size} expected $INPUTS.")
             return HashMap()
         }
 
-        for (i in 0..INPUTS) {
-            this.neurons[i]?.value = inputs[i]
+        for (i in 0..INPUTS-1) {
+            this.neurons[i]?.value = inputs[i].toDouble()
         }
 
         for (neuron in this.neurons.values) {
             var sum = 0.0
-            for (i in 0..neuron.incoming.size) {
+            for (i in 0..neuron.incoming.size-1) {
                 val incoming = neuron.incoming[i]
                 val other = this.neurons[incoming.input]
                 sum += incoming.weight * other!!.value
@@ -32,10 +31,10 @@ class Network {
             }
         }
 
-        val outputs : MutableMap<String, Boolean> = HashMap()
+        val outputs : MutableMap<Int, Boolean> = HashMap()
 
-        for (i in 0..OUTPUT_NAMES.size) {
-            val button = OUTPUT_NAMES[i]
+        for (i in 0..OUTPUT_INDEX.size-1) {
+            val button = OUTPUT_INDEX[i]
             outputs[button] = this.neurons[MAX_NODES + i]!!.value > 0
         }
 
