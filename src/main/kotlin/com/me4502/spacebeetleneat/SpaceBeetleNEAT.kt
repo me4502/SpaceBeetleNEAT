32c9f65e620d710b5c48827c4412ab89c4327a9a
Madeline Miller <mnmiller1@me.com>
2017-03-09 19:48:07 +1000

    Setup the main breeding system.

--- src/main/kotlin/com/me4502/spacebeetleneat/SpaceBeetleNEAT.kt
+++ src/main/kotlin/com/me4502/spacebeetleneat/SpaceBeetleNEAT.kt
@@ -1,21 +1,49 @@
 package com.me4502.spacebeetleneat
 
-val OUTPUT_NAMES = arrayOf("Up", "Down", "Left", "Right")
-val INPUTS = 8
+import com.badlogic.gdx.Input
+import java.awt.Graphics
+import javax.swing.JFrame
+import javax.swing.JPanel
+
+val INPUT_WIDTH = 30
+val INPUT_HEIGHT = 40
+
+val OUTPUT_INDEX = arrayOf(Input.Keys.A, Input.Keys.S, Input.Keys.W, Input.Keys.D)
+val INPUTS = INPUT_WIDTH * INPUT_HEIGHT
 
 val PERTURB_CHANCE = 0.90
 val CROSSOVER_CHANCE = 0.75
 
-val POPULATION = 300
+val POPULATION = 50
 val MAX_NODES = 10000
 
-val TIMEOUT_LIMIT = 20
+val TIMEOUT_LIMIT = 200
+
+val STALE_SPECIES = 15
 
 var gameRunner : GameRunner? = null
 
 fun main(args: Array<String>) {
+    val frame = JFrame()
+    frame.isVisible = true
+
+    //frame.add(CustomJPanel())
+
     gameRunner = GameRunner()
     gameRunner!!.run()
 }
 
 fun sigmoid(x: Double) : Double = (1 / (1 + Math.exp(-x)))
+
+class CustomJPanel : JPanel() {
+
+    override fun paint(g: Graphics?) {
+        super.paint(g)
+
+        for (i in gameRunner!!.getInputs()) {
+            val x = i % INPUT_WIDTH
+            val y = Math.floor(i / INPUT_HEIGHT.toDouble()).toInt()
+            g?.fillRect(x, y, INPUT_WIDTH, INPUT_HEIGHT)
+        }
+    }
+}
